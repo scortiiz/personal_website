c4e00ebe643d23fedcd336d890de0033c1f751d6
sophia <scarazoortiz@berkeley.edu>
2025-01-18 19:32:38 -0800

    pls

--- src/app/lib/cards.js
+++ src/app/lib/cards.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Image from 'next/image';
 import './cards.css';
 
 function Card(props) {
     return(
             <div className = 'card'>
                 <h1 className = "title">{props.title} </h1>
-                <img src={props.imageURL} alt={props.title} className = "pictr"/>
+                <Image src={props.imageURL} alt={props.title} className = "pictr"/>
                 <p className = "desc">{props.value}</p>
             </div>
     )
