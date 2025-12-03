50dae088330a344fabef77853bc59f6b5d52a394
Sophia <scarazoortiz@berkeley.edu>
2025-12-03 13:48:04 -0800

    updating website to scale

--- src/app/components/about/about.js
+++ src/app/components/about/about.js
@@ -1,12 +1,27 @@
-// import React from 'react';
+'use client';
+import { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import './about.css';
 import mepic from '../../../media/mepic.jpg';
 
 export default function About() {
+    const typewriterRef = useRef(null);
+
+    useEffect(() => {
+        // Force animation to run on client-side after mount
+        if (typewriterRef.current) {
+            // Reset and restart animation
+            const element = typewriterRef.current;
+            element.style.animation = 'none';
+            // Force reflow
+            void element.offsetWidth;
+            element.style.animation = '';
+        }
+    }, []);
+
     return (
       <section id="about" className='home'>
-        <div >
+        <div className="photoContainer">
                 <Image
                 className = 'mepic'
                 src = {mepic} alt = "bello"
@@ -14,7 +29,7 @@ export default function About() {
                 />
             </div>
             <div className = 'intro'>
-                <h1 className = 'head'> Hello!</h1>
+                <h1 ref={typewriterRef} className = 'head typewriter'> Hello!</h1>
                 <p className = 'name'> Im Sophia </p>
                 <p className = 'lilintro'> I am a student at UC Berkeley studying Electrical Engineering & Computer Science</p>
             </div>
